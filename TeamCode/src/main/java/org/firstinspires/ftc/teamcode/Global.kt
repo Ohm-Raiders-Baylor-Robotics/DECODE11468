22191b3572ea8f50c0a492e0d98d434b3bc6da8f
Avery Levin <88396486+Avery-Levin@users.noreply.github.com>
2025-09-21 20:23:17 -0400

    Created Global object These are obviously just placeholders for future values, but I wanted to get this part done to begin with.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Global.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode
+
+object Global {
+    val FRONT_LEFT_MOTOR_NAME = "Fl_motor"
+    val FRONT_RIGHT_MOTOR_NAME = "Fr_motor"
+    val BACK_LEFT_MOTOR_NAME = "Bl_motor"
+    val BACK_RIGHT_MOTOR_NAME = "Br_motor"
+
+    val INTAKE_MOTOR_NAME = "intake"
+    val TURRET_MOTOR_NAME = "turret"
+
+    val PINPOINT_NAME = "pinpoint"
+
+
+}
